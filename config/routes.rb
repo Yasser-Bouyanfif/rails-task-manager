9d31c64bb9c23efc8b7597cce13bfdf8462dd722
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2023-06-13 13:22:24 +0200

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
 end
